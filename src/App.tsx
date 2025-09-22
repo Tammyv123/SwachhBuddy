41eca41f362b76c32354aee3a6d86927a2ea7f15
Tamanna <vermatamanna409@gmail.com>
2025-09-22 23:37:18 +0530

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages & Components
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import Landing from "./pages/Landing";
 import Play from "./pages/play";
 import Earn from "./pages/earn";
